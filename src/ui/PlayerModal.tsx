6476478353cdb1517f3a3d9f50ff0e0154b0990a
jugentorba <61308403+jugentorba@users.noreply.github.com>
2026-08-27 18:21:17 +0200

    Seed library snapshot when playback opens

--- src/ui/PlayerModal.tsx
+++ src/ui/PlayerModal.tsx
@@ -1,1 +1,17 @@
-export { PlayerModalNuvio as PlayerModal } from './PlayerModalNuvio';
+import React, { useEffect } from 'react';
+import { PlayerModalNuvio } from './PlayerModalNuvio';
+
+type Props = React.ComponentProps<typeof PlayerModalNuvio>;
+
+export function PlayerModal(props: Props) {
+  useEffect(() => {
+    if (props.progress?.item) return;
+    props.onProgress(
+      props.item,
+      props.progress?.positionSeconds ?? 0,
+      props.progress?.durationSeconds ?? props.item.durationSeconds ?? 0,
+    );
+  }, [props.item, props.onProgress, props.progress]);
+
+  return <PlayerModalNuvio {...props} />;
+}
